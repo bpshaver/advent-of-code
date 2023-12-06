7f1fafbd7721c2ec6bad9dfaca65920a5ffe5192
Ben Shaver <benpshaver@gmail.com>
2023-12-06 10:41:32 -0500

    refactor(23): refactored day 6 to use the quadratic equation. massive speedup!

--- haskell/23_06.hs
+++ haskell/23_06.hs
@@ -1,17 +1,22 @@
-races = [(49, 263), (97, 1532), (94, 1378), (94,1851)]
+races = [(49, 263), (97, 1532), (94, 1378), (94, 1851)]
 
-listDistances totalTime = [ (totalTime - t) * t | t <- [0..totalTime]]
+listDistances totalTime = [(totalTime - t) * t | t <- [0 .. totalTime]]
 
 listWinningTimes totalTime distance = filter (> distance) (listDistances totalTime)
 
 numWinningTimes totalTime distance = length (listWinningTimes totalTime distance)
 
-partOne = product(map (uncurry numWinningTimes) races)
+partOne = product (map (uncurry numWinningTimes) races)
 
-partTwo = numWinningTimes 49979494 263153213781851
+partTwo' = numWinningTimes 49979494 263153213781851
+
+quadSolution time distance =
+  let sqrtComponent = sqrt (time ^ 2 - (4 * (distance + 1)))
+   in floor ((time + sqrtComponent) / 2) - ceiling ((time - sqrtComponent) / 2) + 1
+
+partTwo = quadSolution 49979494 263153213781851
 
 main = do
   print partOne
 
   print partTwo
-
--- haskell/README.md
+++ haskell/README.md
@@ -2,12 +2,28 @@
 
 ## 2023 Day 6
 
-```bash
-$ time ( ghc 23_06.hs && ./23_06 )
-4403592
-38017587
+List comprehension and filter, refactored to use quadratic equation to find roots for polynomial.
 
-real	0m2.142s
-user	0m2.106s
-sys	0m0.023s
+- Part One: (0.01 secs, 179,912 bytes)
+
+- Part Two: (15.36 secs, 16,523,149,904 bytes)
+
+- Part Two (refactored): (0.00 secs, 73,224 bytes)
+
+```ghci
+GHCi, version 9.4.7: https://www.haskell.org/ghc/  :? for help
+ghci> :set +s
+ghci> :l 23_06
+[1 of 2] Compiling Main             ( 23_06.hs, interpreted )
+Ok, one module loaded.
+(0.02 secs,)
+ghci> partOne
+4403592
+(0.01 secs, 179,912 bytes)
+ghci> partTwo
+38017587
+(0.00 secs, 73,224 bytes)
+ghci> partTwo'
+38017587
+(15.36 secs, 16,523,149,904 bytes)
 ```
